8519751fc2c7b1d2320a1dda9811ec47241abc9b
wyozi <joonasmertanen@gmail.com>
2019-03-17 16:20:17 +0200

    Add crappy async debugging

--- pg/src/query.hpp
+++ pg/src/query.hpp
@@ -9,27 +9,42 @@ using namespace GarrysMod::Lua;
 class DatabaseQuery : public LuaEventEmitter<239, DatabaseQuery> {
 private:
   std::string query_string = "";
+  bool debug = false;
 public:
   pqxx::connection* _connection;
   QueryManager* manager;
   
   DatabaseQuery(std::string query_string) : LuaEventEmitter() {
     this->query_string = query_string;
+    AddMethod("enable_debug", enable_debug);
     AddMethod("run", run);
     AddMethod("run_sync", run_sync);
+  }
+
+  LUA_METHOD(enable_debug) {
+    CHECK_CONNECTION
+
+    obj->debug = true;
+
+    return 0;
   }
 
   LUA_METHOD(run) {
     CHECK_CONNECTION
 
-    std::thread([](auto t, auto fut) {
+    std::string query = obj->query_string;
+    bool debug = obj->debug;
+
+    std::thread([debug, query](auto t, auto fut) {
+      if (debug) printf("[PGQuery] waiting for result of %s\n", query.c_str());
       auto ret = fut.get();
+      if (debug) printf("[PGQuery] got result for %s: %d\n", query.c_str(), ret.index());
       if (ret.index() == 0) {
         t->Emit("success", std::get<LuaValue::table_t>(ret));
       } else {
         t->Emit("error", std::get<std::string>(ret));
       }
-    }, obj, obj->manager->query(obj->query_string)).detach();
+    }, obj, obj->manager->query(query)).detach();
 
     return 0;
   }
